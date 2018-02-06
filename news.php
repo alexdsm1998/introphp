0e2060724129f54cf62953076917e316285882e1
aldema <alexdelsaz@gmail.com>
2018-02-06 13:21:09 +0100

    diamonds on my wrist

---
+++ news.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color:orange;
+}
+li {
+    float: right;
+}
+li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+li a:hover {
+    background-color: #F7FE2E;
+}
+</style>
+</head>
+<body>
+
+<ul>
+
+  <li><a href="home.php">Home</a></li>
+  <li><a href="news.php">News</a></li>
+  <li><a href="contact.php">Contact</a></li>
+</ul>
+
+</body>
+</html>
